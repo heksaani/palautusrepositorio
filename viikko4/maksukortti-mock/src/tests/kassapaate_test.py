c0434a600db5027651189c41ed800497d10c9cf4
Heidi Putkuri <heidi.putkuri@helsinki.fi>
2023-11-21 21:07:19 +0200

    Week4 E2

---
+++ viikko4/maksukortti-mock/src/tests/kassapaate_test.py
@@ -0,0 +1,27 @@
+import unittest
+from unittest.mock import Mock, ANY
+from kassapaate import Kassapaate, HINTA
+from maksukortti import Maksukortti
+
+
+class TestKassapaate(unittest.TestCase):
+    def setUp(self):
+        self.kassa = Kassapaate()
+
+    def test_kortilta_velotetaan_hinta_jos_rahaa_on(self):
+        """Kortilta velotetaan hinta jos rahaa on"""
+        maksukortti_mock = Mock()
+        maksukortti_mock.saldo = 10
+
+        self.kassa.osta_lounas(maksukortti_mock)
+
+        maksukortti_mock.osta.assert_called_with(HINTA)
+
+    def test_kortilta_ei_veloteta_jos_raha_ei_riita(self):
+        """Testitapaus: Kortilta ei veloteta jos raha ei riitä"""
+        maksukortti_mock = Mock()
+        maksukortti_mock.saldo = 4
+
+        self.kassa.osta_lounas(maksukortti_mock)
+
+        maksukortti_mock.osta.assert_not_called()
