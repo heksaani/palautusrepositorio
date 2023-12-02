192baf30f235b126d234e2b5ce094248e91a6927
Heidi Putkuri <heidi.putkuri@helsinki.fi>
2023-12-02 15:34:28 +0200

    W5 e4

--- viikko5/laskin/src/sovelluslogiikka.py
+++ viikko5/laskin/src/sovelluslogiikka.py
@@ -1,14 +1,18 @@
 class Sovelluslogiikka:
     def __init__(self, arvo=0):
         self._arvo = arvo
+        self._edelliset_arvot = []
 
     def miinus(self, operandi):
+        self.tallenna_arvo()
         self._arvo = self._arvo - operandi
 
     def plus(self, operandi):
+        self.tallenna_arvo()
         self._arvo = self._arvo + operandi
 
     def nollaa(self):
+        self.tallenna_arvo()
         self._arvo = 0
 
     def aseta_arvo(self, arvo):
@@ -17,5 +21,11 @@ class Sovelluslogiikka:
     def arvo(self):
         return self._arvo
     
+    def tallenna_arvo(self):
+        self._edelliset_arvot.append(self._arvo)
+
     def kumoa(self):
-        pass
+        """Kumoaa edellisen operaation."""
+        edellinen_arvo = self._edelliset_arvot.pop()
+        self.aseta_arvo(edellinen_arvo)
+
